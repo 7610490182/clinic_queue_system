f59054884a56ca11053b6be2b25ebaf45c9dd61d
7610490182 <kushwahamit7610@gmail.com>
2026-05-30 12:50:57 +0530

    port changed in server.js

--- server/server.js
+++ server/server.js
@@ -58,5 +58,5 @@ app.set('io', io);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, 'localhost', () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
